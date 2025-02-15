1cbeeafc7b6402456a9555150dd643a0baeb88b5
dmuchai <dmmuchai@gmail.com>
2025-02-16 00:14:51 +0300

    changes on cities

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -9,7 +9,12 @@ from models import storage
 from models.city import City
 from models.state import State
 
-@app_views.route('/states/<state_id>/cities', methods=['GET'], strict_slashes=False)
+
+@app_views.route(
+        '/states/<state_id>/cities',
+        methods=['GET'],
+        strict_slashes=False
+        )
 def get_cities(state_id):
     """
     Retrieve all cities of a given state
@@ -19,6 +24,7 @@ def get_cities(state_id):
         abort(404)  # If state_id is invalid, return 404
     return jsonify([city.to_dict() for city in state.cities])  # List of cities
 
+
 @app_views.route('/cities/<city_id>', methods=['GET'], strict_slashes=False)
 def get_city(city_id):
     """Retrieve a single city by ID"""
@@ -27,7 +33,12 @@ def get_city(city_id):
         abort(404)  # If city_id is invalid, return 404
     return jsonify(city.to_dict())
 
-@app_views.route('/states/<state_id>/cities', methods=['POST'], strict_slashes=False)
+
+@app_views.route(
+        '/states/<state_id>/cities',
+        methods=['POST'],
+        strict_slashes=False
+        )
 def create_city(state_id):
     """Create a new City in a given State"""
     state = storage.get(State, state_id)
@@ -43,6 +54,7 @@ def create_city(state_id):
     new_city = City(name=request.json['name'], state_id=state_id)
     new_city.save()
     return jsonify(new_city.to_dict()), 201  # Return new city with status 201
+
 
 @app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
 def update_city(city_id):
@@ -60,6 +72,7 @@ def update_city(city_id):
 
     city.save()
     return jsonify(city.to_dict()), 200  # Return updated city
+
 
 @app_views.route('/cities/<city_id>', methods=['DELETE'], strict_slashes=False)
 def delete_city(city_id):
