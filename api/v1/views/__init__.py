240483221f37712ca621c4d12d6330808246d5ee
lensonlovescode <lensonisinparis@gmail.com>
2025-02-17 19:18:47 +0300

    Document

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,6 +2,8 @@
 """
 Contains a python script that creates a flask blueprint
 named app_views as part of a larger flask application
+contains the folllowing views: index, states, cities
+amenities, uusers and places
 """
 from flask import Blueprint
 
@@ -10,5 +12,6 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
+from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
